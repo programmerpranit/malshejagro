20b6fec87402a38ee9c5e4ad130693648c88b162
Pranit <dev.pranitpatil@gmail.com>
2023-11-24 20:36:48 +0530

    side bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,17 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <div className="fixed top-96 right-1">
+        <div className="bg-heading p-3 my-1">
+          <div className="border p-2"></div>
+        </div>
+        <div className="bg-heading p-3 my-1">
+          <div className="border p-2"></div>
+        </div>
+        <div className="bg-heading p-3 my-1">
+          <div className="border p-2"></div>
+        </div>
+      </div>
       <Component {...pageProps} />
     </>
   );
