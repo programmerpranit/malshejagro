17e8ecbb023a96b427261c4bfc32aefe8205af01
Pranit Patil <dev.pranitpatil@gmail.com>
2024-02-22 20:36:08 +0530

    facilities changes

--- src/sections/FacilitiesSection.tsx
+++ src/sections/FacilitiesSection.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { FaChessKing, FaSwimmer } from "react-icons/fa";
-import { MdSportsCricket } from "react-icons/md";
+import { MdSportsCricket, MdPets } from "react-icons/md";
+import { FiCloudRain } from "react-icons/fi";
+import { IoMdBonfire } from "react-icons/io";
+import { GiMicrophone } from "react-icons/gi";
+import { HiMiniSparkles } from "react-icons/hi2";
 const FacilitiesSection = () => {
   return (
     <>
@@ -11,7 +15,7 @@ const FacilitiesSection = () => {
           We have number of activities for every age group to stay active during
           the vacation.{" "}
         </h2>
-        <div className="flex flex-wrap py-10">
+        <div className="flex flex-wrap justify-center py-10">
           <Facility
             title={"Indoor Games"}
             desc="Our variety of Indoor Games are for those who want to make the most of their vacation."
@@ -26,6 +30,31 @@ const FacilitiesSection = () => {
             title={"Swimming Pool"}
             desc="A dive in a clear and clean water on a sunny day is bound to trace away all your blues."
             icon={<FaSwimmer size={60} color={"#395846"} />}
+          />
+          <Facility
+            title={"Rain Dance"}
+            desc="Experience exhilarating fun under cascading water jets in our vibrant Rain Dance area."
+            icon={<FiCloudRain size={60} color={"#395846"} />}
+          />
+          <Facility
+            title={"Pet Friendly"}
+            desc="Welcome pets to our resort, where furry friends are treated like family in our pet-friendly accommodations."
+            icon={<MdPets size={60} color={"#395846"} />}
+          />
+          <Facility
+            title={"Bonfire"}
+            desc="Gather around the crackling flames for cozy evenings of storytelling and camaraderie at our enchanting bonfire pit."
+            icon={<IoMdBonfire size={60} color={"#395846"} />}
+          />
+          <Facility
+            title={"Karaoke"}
+            desc="Unleash your inner superstar and sing your heart out at our lively karaoke nights, filled with music and fun."
+            icon={<GiMicrophone size={60} color={"#395846"} />}
+          />
+          <Facility
+            title={"Wedding Venue"}
+            desc="Marry in scenic beauty at our elegant wedding venue, where dreams come true."
+            icon={<HiMiniSparkles size={60} color={"#395846"} />}
           />
         </div>
       </div>
